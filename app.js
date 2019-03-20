44e530f44aff8773d095ff9bc7d0243c8f0e6f93
Stephane Doiron <stephane.doiron@b367.ca>
2019-03-20 12:00:55 -0400

    ship CRUD

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
+require('dotenv').config();
 var express = require('express');
 var path = require('path');
 // var cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser')
 var logger = require('morgan');
-
+require('./config/database');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const shipsRouter = require('./routes/ships');
 
 var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/ships', shipsRouter);
 
 module.exports = app;
